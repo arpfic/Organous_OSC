7f642fa8857ffe589d2aa530cbc348f6edddcbf3
dleb <damienleb@protonmail.com>
2020-05-07 18:26:48 +0200

    pd up

--- Hardware/python_udpsend.py
+++ Hardware/python_udpsend.py
@@ -9,8 +9,8 @@ port = 12345
 
 osc = OSCClient(address, port)
 for i in range(100000):
-  osc.send_message(b'/main/count', [1])
-#  osc.send_message(b'/main/coil', [0, 40])
+#  osc.send_message(b'/tools/count', [1])
+  osc.send_message(b'/main/motor', [2, 3, -1.0])
 #  time.sleep(100 * 10**(-6))
 #  osc.send_message(b'/main/coil', [0, 0])
-  time.sleep(15 * 10**(-6))
+  time.sleep(2000 * 10**(-6))
